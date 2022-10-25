e195337551f650e979f4a6b07deee98234d8803c
Yesitha Sathsara <60166952+yesitha@users.noreply.github.com>
2022-10-25 08:50:41 +0530

    table scrollbar added

--- src/Items.js
+++ src/Items.js
@@ -6,6 +6,7 @@ import TableCell from '@mui/material/TableCell';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Title from './Title';
+import'./Items.css';
 
 // Generate Order Data
 function createData(id, date, name,vendor, qty,reOrderLevel, valuatintMethod, value) {
